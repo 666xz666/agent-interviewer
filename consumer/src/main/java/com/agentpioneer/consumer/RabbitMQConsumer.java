539d67286dc147714e5641c6b7908fccc90fa324
666xz666 <2965962144@qq.com>
2025-06-11 02:25:48 +0800

    [feat] add consumer layer

---
+++ consumer/src/main/java/com/agentpioneer/consumer/RabbitMQConsumer.java
@@ -0,0 +1,13 @@
+package com.agentpioneer.consumer;
+
+import com.agentpioneer.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConsumer {
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void receiveMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
