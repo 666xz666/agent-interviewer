6255c18be209c20fdbb21c9c7fedb15fdde4a4fb
666xz666 <2965962144@qq.com>
2025-06-15 00:19:44 +0800

    [docs] update README

---
+++ service/src/main/java/com/agentpioneer/service/UserService.java
@@ -0,0 +1,15 @@
+package com.agentpioneer.service;
+
+import com.agentpioneer.pojo.User;
+import com.agentpioneer.pojo.bo.AuthBO;
+import com.agentpioneer.pojo.bo.UserUpdateBO;
+import org.springframework.stereotype.Service;
+import com.agentpioneer.result.BusinessException;
+
+@Service
+public interface UserService {
+    boolean register(AuthBO authBO);
+    User validate(AuthBO authBO);
+    User getUserInfo(Long userId);
+    void updateUserInfo(UserUpdateBO userUpdateBO) throws BusinessException;
+}
